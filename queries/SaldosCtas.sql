ad50313da13051a20b586d8adac83cfec87a6cdf
Mapaznavarro <165283247+Mapaznavarro@users.noreply.github.com>
2026-04-13 19:06:44 +0200

    Refactor SQL query to improve readability and filters

--- queries/SaldosCtas.sql
+++ queries/SaldosCtas.sql
@@ -1,4 +1,4 @@
-select EntPar.codigo, EntAdm.rut, EntPar.Nombre, EntPar.Apellidos , fon.fondo, fon.planes, par.secuencia, par.regimen_tributario, Cta.codigo,   sal.participaciones SaldoCuota from 
+select EntPar.codigo as titular, EntAdm.rut, EntPar.Nombre, EntPar.Apellidos , fon.fondo, fon.planes, par.secuencia, par.regimen_tributario, Cta.codigo as cuenta,   sal.participaciones SaldoCuota from 
                  PAR_SALDOS Sal
                , cuentas_tit Cta 
                , cuentas_par Par
@@ -10,13 +10,8 @@ where Ent.Codigo = Cta.Codigo
 and   Par.Codigo = Cta.codigo
 and   Sal.cuenta = Cta.Codigo
 and   Fon.Cuenta = Cta.Codigo
-and   Sal.participaciones<> 0
--- and   Cta.titular = 1666859
--- and   EntAdm.rut = 15664514
---and Cta.Codigo = 87962713
+and   Cta.titular >= 2962322 and Cta.titular <= 2962400
 and   EntPar.codigo = Cta.titular
 and   EntAdm.codigo = EntPar.codigo
--- and   fon.planes = 3002
---and par.regimen_tributario <> 'A'
---order by sal.participaciones 
+order by EntPar.codigo
 ;
